324013454a3a11453bbb80b08524323beb9c7d3a
Jacob Harrington <jharrington4@gmail.com>
2020-02-13 14:19:23 -0800

    store created and provider made

--- src/reducers/moodReducer.js
+++ src/reducers/moodReducer.js
@@ -1,6 +1,13 @@
 import { DRINK_COFFEE, EAT_SNACK, TAKE_NAP, STUDY } from '../actions/moodActions';
 
-export default function reducer(state, action){
+const initialState = {
+  coffees: 0,
+  snacks: 0,
+  naps: 0,
+  studies: 0
+};
+
+export default function reducer(state = initialState, action){
   switch(action.type){
     case DRINK_COFFEE:
       return { ...state, coffees: state.coffees + 1 };
