1a5ec30200ddef64225ec2705e80a26f54333b76
Jacob Harrington <jharrington4@gmail.com>
2020-02-13 14:55:07 -0800

    faceSelector complete and tests passing

--- src/containers/Moods.js
+++ src/containers/Moods.js
@@ -3,33 +3,17 @@ import Controls from '../components/controls/Controls';
 import Face from '../components/face/Face';
 import { useDispatch, useSelector } from 'react-redux';
 import { drinkCoffee, eatSnack, takeNap, toStudy } from '../actions/moodActions';
-
-export const isTired = state => state.coffees < 1 && state.naps < 1;
-export const isHyper = state => state.coffees > 3;
-export const isEducated = state => state.studies > 2;
-export const isHungry = state => state.snacks < 1;
-
-export const getFace = state => {
-  if(isTired(state) && isHungry(state)) return '🤬';
-  if(isHyper(state) && isHungry(state)) return '🤮';
-  if(isTired(state)) return '😴';
-  if(isHyper(state)) return '🙀';
-  if(isEducated(state)) return '🤯';
-  if(isHungry(state)) return '😡';
-
-  return '😀';
-};
+import { getFace } from '../selectors/faceSelector';
 
 export const Moods = () => {
   const state = useSelector(state => state);
   const { coffees, snacks, naps, studies } = state;
   const dispatch = useDispatch();
+  const face = useSelector(getFace);
 
   const handleSelection = action => {
     dispatch(action);
   };
-
-  const face = getFace(state);
 
   return (
     <>
