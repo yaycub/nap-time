c7e3d33d75679a6eb0fe346ee03d111cb1ce68b8
Jacob Harrington <jharrington4@gmail.com>
2020-02-13 17:15:59 -0800

    updated mood.test.js

--- src/containers/Moods.test.js
+++ src/containers/Moods.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { Moods } from './Moods';
 import { Provider } from 'react-redux';
 import store from '../store';
 
 describe('Moods Container', () => {
   it('matches a snapshot', () => {
-    const wrapper = shallow(
+    const wrapper = mount(
       <Provider store={store}>
         <Moods />
       </Provider>
